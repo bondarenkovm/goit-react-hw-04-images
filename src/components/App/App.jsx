66da4b167306a5707f56317aa6f4a5bfe539658a
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-03-09 13:49:34 +0200

    refactoring ImageGallery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,103 @@
 import { Component } from 'react';
-import { Toaster } from 'react-hot-toast';
+import { Toaster, toast } from 'react-hot-toast';
 import { Container } from './App.styled';
 import { Searchbar } from 'components/Searchbar';
 import { ImageGallery } from 'components/ImageGallery';
+import { Button } from 'components/Button';
+import { Loader } from 'components/Loader';
+import { fetchPixabay } from 'services/fetchPixabayAPI';
 
 class App extends Component {
   state = {
     imageName: '',
+    items: [],
+    page: 1,
+    showButton: false,
+    loading: false,
   };
 
   formSubmitHandler = imageName => {
     this.setState({ imageName });
   };
 
+  buttonСlick = () => {
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+      };
+    });
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    // const { value } = this.props;
+    const { page, imageName, items } = this.state;
+
+    // if (prevProps.value !== value) {
+    //   this.setState({ imageName: value });
+    // }
+
+    if (prevState.imageName !== imageName) {
+      this.setState({
+        items: [],
+        page: 1,
+        showButton: false,
+      });
+    }
+
+    if (
+      (prevState.imageName !== imageName && page === 1) ||
+      (prevState.imageName === imageName && prevState.page !== page)
+    ) {
+      try {
+        this.setState({ loading: true });
+        const { hits, totalHits } = await fetchPixabay(imageName, page);
+
+        this.setState({ loading: false });
+        if (hits.length) {
+          this.setState({ showButton: true });
+          this.setState(prevState => {
+            return {
+              items: [...prevState.items, ...hits],
+            };
+          });
+        } else {
+          toast.error('Nothing was found according to the search results!', {
+            style: {
+              background: '#ca1616',
+              color: '#fff',
+            },
+          });
+
+          this.setState({
+            items: [],
+            page: 1,
+            showButton: false,
+          });
+        }
+        if (12 + items.length >= totalHits) {
+          this.setState({
+            showButton: false,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   render() {
     const {
-      state: { imageName },
+      state: { items, loading, showButton },
       formSubmitHandler,
+      buttonСlick,
     } = this;
 
     return (
       <Container>
         <Searchbar formSubmit={formSubmitHandler} />
-        <ImageGallery value={imageName} />
+        {loading && <Loader />}
+        <ImageGallery items={items} />
+        {showButton && <Button buttonСlick={buttonСlick} />}
         <Toaster
           toastOptions={{
             duration: 1500,
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,112 +1,113 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { toast } from 'react-hot-toast';
+// import { toast } from 'react-hot-toast';
 import { List, Item } from './ImageGallery.styled';
-import { fetchPixabay } from 'services/fetchPixabayAPI';
+// import { fetchPixabay } from 'services/fetchPixabayAPI';
 import { ImageGalleryItem } from 'components/ImageGalleryItem';
-import { Button } from 'components/Button';
-import { Loader } from 'components/Loader';
+// import { Button } from 'components/Button';
+// import { Loader } from 'components/Loader';
 
-class ImageGallery extends Component {
-  state = {
-    imageName: '',
-    items: [],
-    page: 1,
-    showButton: false,
-    loading: false,
-  };
+function ImageGallery({ items }) {
+  // state = {
+  // imageName: '',
+  // items: [],
+  // page: 1,
+  // showButton: false,
+  // loading: false,
+  // };
 
-  buttonСlick = () => {
-    this.setState(prevState => {
-      return {
-        page: prevState.page + 1,
-      };
-    });
-  };
+  // buttonСlick = () => {
+  //   this.setState(prevState => {
+  //     return {
+  //       page: prevState.page + 1,
+  //     };
+  //   });
+  // };
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { value } = this.props;
-    const { page, imageName, items } = this.state;
+  // async componentDidUpdate(prevProps, prevState) {
+  //   const { value } = this.props;
+  //   const { page, imageName, items } = this.state;
 
-    if (prevProps.value !== value) {
-      this.setState({ imageName: value });
-    }
+  //   if (prevProps.value !== value) {
+  //     this.setState({ imageName: value });
+  //   }
 
-    if (prevState.imageName !== imageName) {
-      this.setState({
-        items: [],
-        page: 1,
-        showButton: false,
-      });
-    }
+  //   if (prevState.imageName !== imageName) {
+  //     this.setState({
+  //       items: [],
+  //       page: 1,
+  //       showButton: false,
+  //     });
+  //   }
 
-    if (
-      (prevProps.value !== value && page === 1) ||
-      (prevProps.value === value && prevState.page !== page)
-    ) {
-      try {
-        this.setState({ loading: true });
-        const { hits, totalHits } = await fetchPixabay(value, page);
+  //   if (
+  //     (prevProps.value !== value && page === 1) ||
+  //     (prevProps.value === value && prevState.page !== page)
+  //   ) {
+  //     try {
+  //       this.setState({ loading: true });
+  //       const { hits, totalHits } = await fetchPixabay(value, page);
 
-        this.setState({ loading: false });
-        if (hits.length) {
-          this.setState({ showButton: true });
-          this.setState(prevState => {
-            return {
-              items: [...prevState.items, ...hits],
-            };
-          });
-        } else {
-          toast.error('Nothing was found according to the search results!', {
-            style: {
-              background: '#ca1616',
-              color: '#fff',
-            },
-          });
+  //       this.setState({ loading: false });
+  //       if (hits.length) {
+  //         this.setState({ showButton: true });
+  //         this.setState(prevState => {
+  //           return {
+  //             items: [...prevState.items, ...hits],
+  //           };
+  //         });
+  //       } else {
+  //         toast.error('Nothing was found according to the search results!', {
+  //           style: {
+  //             background: '#ca1616',
+  //             color: '#fff',
+  //           },
+  //         });
 
-          this.setState({
-            items: [],
-            page: 1,
-            showButton: false,
-          });
-        }
-        if (12 + items.length >= totalHits) {
-          this.setState({
-            showButton: false,
-          });
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
+  //         this.setState({
+  //           items: [],
+  //           page: 1,
+  //           showButton: false,
+  //         });
+  //       }
+  //       if (12 + items.length >= totalHits) {
+  //         this.setState({
+  //           showButton: false,
+  //         });
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  // }
 
-  render() {
-    const { items, showButton, loading } = this.state;
-    return (
-      <>
-        {loading && <Loader />}
-        <List>
-          {items.map(({ id, webformatURL, largeImageURL, user }) => {
-            return (
-              <Item key={id}>
-                <ImageGalleryItem
-                  bigPicture={largeImageURL}
-                  smallImage={webformatURL}
-                  alt={user}
-                />
-              </Item>
-            );
-          })}
-        </List>
+  // render() {
+  // const { items } = this.props;
+  return (
+    <>
+      {/* {loading && <Loader />} */}
+      <List>
+        {items.map(({ id, webformatURL, largeImageURL, user }) => {
+          return (
+            <Item key={id}>
+              <ImageGalleryItem
+                bigPicture={largeImageURL}
+                smallImage={webformatURL}
+                alt={user}
+              />
+            </Item>
+          );
+        })}
+      </List>
 
-        {showButton && <Button buttonСlick={this.buttonСlick} />}
-      </>
-    );
-  }
+      {/* {showButton && <Button buttonСlick={this.buttonСlick} />} */}
+    </>
+  );
+  // }
 }
 
 ImageGallery.propTypes = {
-  value: PropTypes.string.isRequired,
+  // value: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
 };
 export { ImageGallery };
