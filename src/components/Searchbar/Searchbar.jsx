f5b603fbcd36b1115c0539281760f8d7cb4050fc
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-03-11 12:50:31 +0200

    fix

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -40,62 +40,6 @@ const Searchbar = ({ formSubmit }) => {
   );
 };
 
-//-------------------------class---------------------------
-// class Searchbar extends Component {
-//   state = {
-//     imageName: '',
-//   };
-//   handleNameChange = ({ currentTarget: { value } }) => {
-//     this.setState({ imageName: value.toLowerCase() });
-//   };
-
-//   handleSubmit = evt => {
-//     const { imageName } = this.state;
-//     const { formSubmit } = this.props;
-//     evt.preventDefault();
-// if (imageName.trim() === '') {
-//   toast.error('Enter something!', {
-//     style: {
-//       background: '#ca1616',
-//       color: '#fff',
-//     },
-//   });
-
-//       this.setState({ imageName: '' });
-//       return;
-//     }
-//     formSubmit(imageName);
-//     this.setState({ imageName: '' });
-//   };
-
-//   render() {
-//     const {
-//       handleNameChange,
-//       handleSubmit,
-//       state: { imageName },
-//     } = this;
-//     return (
-//       <Header>
-//         <Form onSubmit={handleSubmit}>
-//           <Button type="submit">
-//             <FcSearch style={{ width: 40, height: 40 }} />
-//             {/* <Span>Search</Span> */}
-//           </Button>
-
-//           <Input
-//             type="text"
-//             value={imageName}
-//             name="imageName"
-//             autocomplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//             onChange={handleNameChange}
-//           />
-//         </Form>
-//       </Header>
-//     );
-//   }
-// }
 Searchbar.propTypes = {
   formSubmit: PropTypes.func.isRequired,
 };
